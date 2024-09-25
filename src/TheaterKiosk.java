cd414d282b887d38507099a0b34ed2c5f75d021b
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-25 08:20:47 -0400

    Initial commit

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,22 @@
-public class Main {
+import java.util.Scanner;
+
+public class TheaterKiosk {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan  = new Scanner(System.in);
+        
+        int age;
+        System.out.println("What is your age? ");
+        if (scan.hasNextInt()){
+            age = scan.nextInt();
+            if(age >= 21 && age <= 122) {
+                System.out.println("You get a wristband");
+                
+            } else {
+                System.exit(0);
+            }
+        } else {
+            System.out.println("You entered a wrong data type");
+            System.exit(0);
+        }
     }
 }
